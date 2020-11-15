dcfa5b6c7d41923c128cb812e24b6e44c75bf68b
Delfim Pereira <delfimgp@hotmail.com>
2020-11-15 20:04:31 +0000

    Maior ou menor

--- app/src/main/java/pt/delfimpereira/adivinha/MainActivity.java
+++ app/src/main/java/pt/delfimpereira/adivinha/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -12,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
     private Random random= new Random();
     private int numeroAdivinhar;
     private int tentativas;
+
+    private EditText editTextNumero = null;
+    private TextView textViewAcertou = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +31,47 @@ public class MainActivity extends AppCompatActivity {
         tentativas = 0;
     }
 
+    private EditText getEditTextNumero() {
+        if (editTextNumero == null) {
+            EditText editTextNumero = findViewById(R.id.editTextNumero);
+        }
+
+        return  editTextNumero;
+    }
+
+    private TextView getTextViewAcertou() {
+        if (textViewAcertou == null) {
+            textViewAcertou = findViewById(R.id.textViewAcertou);
+        }
+
+        return textViewAcertou;
+    }
+
     public void adivinha(View view) {
         int numero = getNumero();
 
+        if (numero < 1 || numero > 10) {
+            EditText editTextNumero = getEditTextNumero();
+            editTextNumero.setError(getString(R.string.numero_invalido));
+            return;
+        }
+
+        if (numero == numeroAdivinhar) {
+            getTextViewAcertou().setText(R.string.acertou);
+        } else if (numero < numeroAdivinhar) {
+            getTextViewAcertou().setText(R.string.numero_maior);
+        }
+        else {
+            getTextViewAcertou().setText(R.string.numero_menor);
+        }
     }
 
     private int getNumero() {
-        EditText editTextNumero = findViewById(R.id.editTextNumero);
-
-        String s = editTextNumero.getText().toString();
+        String s = getEditTextNumero().getText().toString();
 
         try {
             return Integer.parseInt(s);
         } catch (NumberFormatException e) {
-            editTextNumero.setError("Número inválido. Introduza um número entre 1 e 10");
             e.printStackTrace();
             return 0;
         }
